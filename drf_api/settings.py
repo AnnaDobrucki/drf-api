89c4da9e7cc10bba0b28dda257d5f3b60eceb5f6
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2024-02-02 13:17:52 +0000

    debug true

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','https://django-rest-framework-dobrucki-531ba6a81825.herokuapp.com', '8000-annadobrucki-drfapi-uxe8w9eq0s6.ws-eu107.gitpod.io']
 
